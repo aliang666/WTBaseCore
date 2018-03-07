d061bc08e1251e419387ba6675b797b92576c0d0
elji <elji@flytek.com>
2018-03-07 19:23:37 +0800

    no message

---
+++ WTBaseCoreDemo/WTBaseCoreDemo/WTMineViewController/WTMineViewController.m
@@ -0,0 +1,23 @@
+//
+//  WTCenterViewController.m
+//  WTBaseCoreDemo
+//
+//  Created by admin on 2018/3/7.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import "WTMineViewController.h"
+
+@interface WTMineViewController ()
+
+@end
+
+@implementation WTMineViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navBar.title = @"中间页面";
+    self.navBar.leftItemList = nil;
+}
+
+@end
